6e8c0f4d9afc1e69765a2946d703e63e5bde5f89
Admin <duisenbaidaryn2205@gmail.com>
2023-04-19 21:26:48 +0600

    3 stage

--- MyArrayList.java
+++ MyArrayList.java
@@ -32,4 +32,32 @@ public class MyArrayList<T> implements mylist<T> {
         }
         elements[size++] = item;
     }
+    @Override
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (size == elements.length) {
+            Object[] newElements = new Object[2 * elements.length];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(item)) {
+                System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                elements[--size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
 }
