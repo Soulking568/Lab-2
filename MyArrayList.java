2d3340ea2b49f67ad9696aa9a746c247396cf562
Admin <duisenbaidaryn2205@gmail.com>
2023-04-19 21:26:09 +0600

    2 stage

--- MyArrayList.java
+++ MyArrayList.java
@@ -13,4 +13,23 @@ public class MyArrayList<T> implements mylist<T> {
     public int size() {
         return size;
     }
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+        if (size == elements.length) {
+            Object[] newElements = new Object[2 * elements.length];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        elements[size++] = item;
+    }
 }
