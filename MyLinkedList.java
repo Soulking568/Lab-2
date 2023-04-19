15cfabd4097c84fd7af70cda9aab9da19ac73d63
Admin <duisenbaidaryn2205@gmail.com>
2023-04-19 21:30:39 +0600

    7 stage

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -25,6 +25,31 @@ public class MyLinkedList<T> implements mylist<T> {
     public int size() {
         return size;
     }
+    @Override
+    public boolean contains(Object o) {
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+        Node newNode = new Node(item);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
 
 
 }
